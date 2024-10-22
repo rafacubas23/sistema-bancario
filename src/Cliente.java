3d582e1fc56fa8b3c6009d426ac0298d76bd1e32
rafacubas23 <rafael.cubas.work@gmail.com>
2024-10-22 15:58:30 -0300

    Adicionada a classe "Cliente".

---
+++ src/Cliente.java
@@ -0,0 +1,32 @@
+public class Cliente {
+    private final String nome;
+    private final String cpf;
+    private final String telefone;
+
+    public Cliente(String nome, String cpf, String telefone) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+    }
+
+    public void showInfo(){
+        System.out.println("Nome: " + nome);
+        System.out.println("CPF: " + cpf);
+        System.out.println("Telefone: " + telefone);
+    }
+
+// getters and setters //
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+// end of getters and setters //
+
+}
